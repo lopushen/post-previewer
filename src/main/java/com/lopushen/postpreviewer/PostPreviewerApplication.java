789a638e299e1d460914b214570d30eb52e46711
Ievgenii Lopushen <lopushen@gmail.com>
2019-07-17 21:50:01 +0300

    Initial commit of post previewer

---
+++ src/main/java/com/lopushen/postpreviewer/PostPreviewerApplication.java
@@ -0,0 +1,13 @@
+package com.lopushen.postpreviewer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostPreviewerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostPreviewerApplication.class, args);
+	}
+
+}
